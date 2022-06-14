1e8e616c3e2c3f7b9069b17aa41262503e287089
DeploymentsPractice <deploymentspractice@gmail.com>
2022-06-14 14:50:31 +0530

    error

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,13 @@
-import main from "../assets/images/main.svg";
-import logo from "../assets/images/logo.svg";
-// import { Link } from "react-router-dom";
+import main from "../assets/images/main-alternative.svg";
+import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="logo" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
@@ -20,7 +20,9 @@ const Landing = () => {
             quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
             narwhal.
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
